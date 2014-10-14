5fd4288e602f7e57687231464d2591b0e92d356a
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-10-14 16:41:02 -0600

    Update sale_line_margin.py

--- sale_line_margin.py
+++ sale_line_margin.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*- 
 
 from openerp import models, fields, api
@@ -5,14 +6,14 @@ from openerp import models, fields, api
 class sale_line_margin(models.Model):
 
         _inherit = "sale.order.line"
-      
+
         mar_p = fields.Float(string='% Margen', digits=(6,3),compute='margin_line')
 
 
         @api.one
-        @api.depends('price_unit','purchase_price')
+        @api.depends('price_unit','purchase_price','discount')
         def margin_line(self):
-                if self.price_unit == 0:
+                if self.purchase_price == 0:
                     self.mar_p = 0.0;
                 else:
-                    self.mar_p=(self.price_unit - self.purchase_price)
+                    self.mar_p=(self.price_unit - self.discount*self.price_unit/100 - self.purchase_price)/self.purchase_price*100
